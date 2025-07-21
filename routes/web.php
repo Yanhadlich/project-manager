ee825fb807609277e4bdd184c53ff8ad7f7dfcf8
Yanhadlich <yanhadlich@gmail.com>
2025-07-21 16:56:59 -0300

    Feature: Added crud routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,13 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group( function () {
+    Route::get('/projects', [ProjectsController::class. 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectsController::class. 'create'])->name('projects.create');
+    Route::put('/projects/{project}', [ProjectsController::class. 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectsController::class. 'delete'])->name('projects.delete');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
